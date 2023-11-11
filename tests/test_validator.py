e3c88847081d7f2960e34204f746f308198ee11a
Janine Lapiz <janine@restoplus.com.au>
2023-11-11 23:30:52 +0800

    Added validator

---
+++ tests/test_validator.py
@@ -0,0 +1,100 @@
+from unittest import TestCase
+from utils import validator
+
+class TestValidateStartTime(TestCase):
+    def test_time_is_valid(self):
+        self.assertIsNone(validator.validate_start_time('12:30 AM'))
+        self.assertIsNone(validator.validate_start_time('2:30 AM'))
+        self.assertIsNone(validator.validate_start_time('2:30 PM'))
+        
+    def test_no_meridiem(self):
+        with self.assertRaises(Exception) as context:
+            validator.validate_start_time('12:00')
+        self.assertEqual(str(context.exception), 'Invalid start time pattern')
+    
+    def test_meridiem_is_invalid(self):
+        with self.assertRaises(Exception) as context:
+            validator.validate_start_time('12:00 AA')
+        self.assertEqual(str(context.exception), 'Invalid start time pattern')
+        
+    def test_no_minutes(self):
+        with self.assertRaises(Exception) as context:
+            validator.validate_start_time('12 PM')
+        self.assertEqual(str(context.exception), 'Invalid start time pattern')
+        
+    def test_minutes_is_letters(self):
+        with self.assertRaises(Exception) as context:
+            validator.validate_start_time('12:fsdfsd AM')
+        self.assertEqual(str(context.exception), 'Invalid start time pattern') 
+        
+    def test_minutes_is_more_than_2_digits(self):
+        with self.assertRaises(Exception) as context:
+            validator.validate_start_time('4:450 PM')
+        self.assertEqual(str(context.exception), 'Invalid start time pattern')
+    
+    def test_minutes_is_less_than_2_digits(self):
+        with self.assertRaises(Exception) as context:
+            validator.validate_start_time('12:0 PM')
+        self.assertEqual(str(context.exception), 'Invalid start time pattern')
+    
+    def test_minutes_is_more_than_60(self):
+        with self.assertRaises(Exception) as context:
+            validator.validate_start_time('12:63 PM')
+        self.assertEqual(str(context.exception), 'Invalid minutes')
+            
+    def test_minutes_is_less_than_0(self):
+        with self.assertRaises(Exception) as context:
+            validator.validate_start_time('12:-50 PM')
+        self.assertEqual(str(context.exception), 'Invalid start time pattern')
+
+    def test_hours_is_letters(self):
+        with self.assertRaises(Exception) as context:
+            validator.validate_start_time('sdfs:30 PM')
+        self.assertEqual(str(context.exception), 'Invalid start time pattern')
+    
+    def test_hours_is_more_than_3_digits(self):
+        with self.assertRaises(Exception) as context:
+            validator.validate_start_time('124:30 PM')
+        self.assertEqual(str(context.exception), 'Invalid start time pattern')
+    
+    def test_hours_is_more_than_12(self):
+        with self.assertRaises(Exception) as context:
+            validator.validate_start_time('13:00 AM') 
+        self.assertEqual(str(context.exception), 'Must be a 12-hour format')
+        
+    def test_hours_is_less_than_1(self):
+        with self.assertRaises(Exception) as context_b:
+            validator.validate_start_time('0:00 PM')
+        self.assertEqual(str(context_b.exception), 'Must be a 12-hour format')
+    
+    
+class TestValidateDuration(TestCase):
+    def test_duration_is_valid(self):
+        self.assertIsNone(validator.validate_duration('1:30'))
+        self.assertIsNone(validator.validate_duration('235:00'))
+        self.assertIsNone(validator.validate_duration('0:10'))
+    
+    def test_hours_or_minutes_is_missing(self):
+        with self.assertRaises(Exception) as context:
+            validator.validate_duration('234')
+        self.assertEqual(str(context.exception), 'Invalid duration format')
+    
+    def test_minutes_is_less_than_2_digits(self):
+        with self.assertRaises(Exception) as context:
+            validator.validate_duration('12:1')
+        self.assertEqual(str(context.exception), 'Invalid duration format')
+        
+    def test_minutes_is_more_than_60(self):
+        with self.assertRaises(Exception) as context:
+            validator.validate_duration('12:80')
+        self.assertEqual(str(context.exception), 'Invalid minutes')
+    
+    def test_minutes_is_less_than_0(self):
+        with self.assertRaises(Exception) as context:
+            validator.validate_duration('12:-2')
+        self.assertEqual(str(context.exception), 'Invalid duration format')
+    
+    def test_hours_is_less_than_0(self):
+        with self.assertRaises(Exception) as context:
+            validator.validate_duration('-23:00')
+        self.assertEqual(str(context.exception), 'Invalid duration format')
---
+++ utils/validator.py
@@ -0,0 +1,28 @@
+import re
+
+def validate_start_time(time: str) -> None:
+    start_time_pattern = r'\d{1,2}:\d{2} [AP]M'
+        
+    if not re.match(start_time_pattern, time): raise Exception('Invalid start time pattern')
+    
+    hours_and_minutes = time.split()[0]
+    
+    [hours, minutes] = hours_and_minutes.split(':')
+    
+    if int(hours) > 12 or int(hours) < 1: raise Exception('Must be a 12-hour format')
+        
+    # No need to check if minutes is less than 0. 
+    # The pattern check above ensures it's always an integer.
+    if int(minutes) > 60: raise Exception('Invalid minutes')
+    
+
+def validate_duration(duration: str) -> None:
+    duration_pattern = r'\d+:\d{2}'
+    
+    if not re.match(duration_pattern, duration): raise Exception('Invalid duration format')
+    
+    minutes = duration.split(':')[1]
+    
+    # No need to check if minutes is less than 0. 
+    # The pattern check above ensures it's always an integer.
+    if int(minutes) > 60: raise Exception('Invalid minutes')
